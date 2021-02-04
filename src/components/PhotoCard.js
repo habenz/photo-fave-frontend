9fe333b93602df037ca627f187710bbb169aca8c
Haben Zaid <haben.zaidt@gmail.com>
2021-02-04 01:18:30 -0500

    added styling and button to photo cards

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import styles from './PhotoCard.module.css';
 
 const PhotoCard = ({photo}) => {
@@ -7,6 +10,9 @@ const PhotoCard = ({photo}) => {
 	return (
 		<div className={styles.card}>
 			<img className={styles.img} src={photo.url} alt={photo.name}/>
+			<button className={styles.like}>
+				<FontAwesomeIcon icon={faHeart} />
+			</button>
 		</div>
 	)
 }
