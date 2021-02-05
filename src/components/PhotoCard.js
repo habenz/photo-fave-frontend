e1dc331ff30ddb017b12abc48b8c230b8d4faad8
Haben Zaid <haben.zaidt@gmail.com>
2021-02-05 09:47:05 -0500

    liked photos now have a red buttton

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -7,17 +7,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from './PhotoCard.module.css';
 
 const PhotoCard = ({photo}) => {
-	const userId = useSelector(state => state.login);
+	const userId = useSelector(state => state.login.user._id);
 	console.log('userId', userId);
 
-	const heartColor = "grey"
-	// const heartColor = (photo.likes.includes(userId)) ? "red" : "lightgrey";
-	// console.log("photo likes", photo.likes);
-	// console.log('boolean', photo.likes.includes(userId));
+	const heartColor = (photo.likes.includes(userId)) ? "red" : "lightgrey";
+
+	const likePic = () => {
+		// add functionality here
+	}
+
 	return (
 		<div className={styles.card}>
 			<img className={styles.img} src={photo.url} alt={photo.name}/>
-			<button className={styles.like}>
+			<button className={styles.like} onClick={likePic}>
 				<FontAwesomeIcon icon={faHeart} style={{color: heartColor}}/>
 			</button>
 		</div>
