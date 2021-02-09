9fe1c4fbccf29cf1c17f047bea0bb391484b7bde
Haben Zaid <haben.zaidt@gmail.com>
2021-02-09 17:31:58 -0500

    save redux state in localStorage

--- src/index.js
+++ src/index.js
@@ -9,8 +9,20 @@ import './index.css'
 import reducers from './redux'; // no need to specify index.js
 import App from './App';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
-store.subscribe(() => console.log(store.getState()));
+const oldState = JSON.parse(localStorage.getItem("state"));
+let store;
+if (oldState){
+	store = createStore(reducers, oldState, compose(applyMiddleware(thunk)));
+
+} else {
+	store = createStore(reducers, compose(applyMiddleware(thunk)));
+}
+
+
+store.subscribe(() => {
+	console.log(store.getState());
+	localStorage.setItem("state", JSON.stringify(store.getState()));
+});
 
 ReactDOM.render(
   <React.StrictMode>
